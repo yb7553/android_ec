b69b30299612c514469408300561fef06d7e192a
yb7553 <23218371@qq.com>
2018-06-16 09:24:25 +0800

    the one

--- latte-ec/src/main/java/com/flj/latte/ec/main/discover/DiscoverDelegate.java
+++ latte-ec/src/main/java/com/flj/latte/ec/main/discover/DiscoverDelegate.java
@@ -3,8 +3,11 @@ package com.flj.latte.ec.main.discover;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.AppCompatTextView;
 import android.view.View;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.flj.latte.delegates.LatteDelegate;
 import com.flj.latte.delegates.bottom.BottomItemDelegate;
 import com.flj.latte.delegates.web.WebDelegateImpl;
@@ -13,6 +16,8 @@ import com.flj.latte.delegates.web.WebDelegatePageImpl;
 import com.flj.latte.ec.R;
 import com.flj.latte.ec.main.cart.ShopOrderDelegate;
 import com.flj.latte.ec.main.personal.PersonalDelegate;
+import com.flj.latte.ec.main.personal.address.AddressAddDelegate;
+import com.flj.latte.net.callback.ISuccess;
 import com.flj.latte.util.log.LatteLogger;
 
 import me.yokeyword.fragmentation.anim.DefaultHorizontalAnimator;
@@ -22,8 +27,9 @@ import me.yokeyword.fragmentation.anim.FragmentAnimator;
  * Created by yb
  */
 
-public class DiscoverDelegate extends LatteDelegate {
+public class DiscoverDelegate extends LatteDelegate implements ISuccess,View.OnClickListener {
     private String mUrl= null;
+    private AppCompatTextView mAppCompatTextView=null;
     @Override
     public Object setLayout() {
         return R.layout.delegate_discover;
@@ -41,6 +47,8 @@ public class DiscoverDelegate extends LatteDelegate {
 
     @Override
     public void onBindView(@Nullable Bundle savedInstanceState, @NonNull View rootView) {
+        mAppCompatTextView = $(R.id.web_discovery_container_share);
+        $(R.id.web_discovery_container_share).setOnClickListener(this);
     }
 
     @Override
@@ -55,4 +63,22 @@ public class DiscoverDelegate extends LatteDelegate {
 //    public FragmentAnimator onCreateFragmentAnimator() {
 //        return new DefaultHorizontalAnimator();
 //    }
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.icon_address_add) {
+            onClickDiscoveryShare();
+        }
+    }
+
+    private void onClickDiscoveryShare() {
+
+
+
+    }
+
+    @Override
+    public void onSuccess(String response) {
+
+    }
 }
