1a0f6f857edaa99c7d15ff49e723fa3575d99b19
yb7553 <23218371@qq.com>
2018-06-18 01:32:30 +0800

    the one test

--- latte-ec/src/main/java/com/flj/latte/ec/main/personal/PersonalDelegate.java
+++ latte-ec/src/main/java/com/flj/latte/ec/main/personal/PersonalDelegate.java
@@ -72,6 +72,8 @@ public class PersonalDelegate extends BottomItemDelegate {
         mArgs = new Bundle();
     }
 
+
+
     @Override
     public void onBindView(@Nullable Bundle savedInstanceState, @NonNull View rootView) {
 
